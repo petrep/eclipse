f1431843a06725d04d029de041ac9a986ec3909c
Peter Peter <petrep@freemail.hu>
2015-10-11 18:33:03 +0200

    new version changes have been made to upload class

--- FinnishGame/src/Menu.java
+++ FinnishGame/src/Menu.java
@@ -17,128 +17,128 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
-public class Menu extends JFrame{
-    JFileChooser fileChooser = new JFileChooser();
-    
-    public Menu() {
+public class Menu extends JFrame {
+	JFileChooser fileChooser = new JFileChooser();
 
-    }
+	public Menu() {
 
-    JMenuBar createMenuBar() {
-        JMenuBar menuBar = new JMenuBar();
-        menuBar.setBackground(Color.WHITE);
+	}
 
-        JMenu fileMenu = new JMenu("File");
-        JMenuItem createFile = new JMenuItem("Create a new file");
-        JMenuItem loadFile = new JMenuItem("Load from file");
-        JMenuItem exitItem = new JMenuItem("Exit");
+	JMenuBar createMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(Color.WHITE);
 
-        fileMenu.add(createFile);
-        fileMenu.add(loadFile);
-        fileMenu.addSeparator();
-        fileMenu.add(exitItem);
-        fileMenu.setMnemonic(KeyEvent.VK_F);
-        exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
-                ActionEvent.ALT_MASK)); // displays the shortcut
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem createFile = new JMenuItem("Create a new file");
+		JMenuItem loadFile = new JMenuItem("Load from file");
+		JMenuItem exitItem = new JMenuItem("Exit");
 
-        
-        JMenu toolsMenu = new JMenu("Tools");
+		fileMenu.add(createFile);
+		fileMenu.add(loadFile);
+		fileMenu.addSeparator();
+		fileMenu.add(exitItem);
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+		exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
+				ActionEvent.ALT_MASK)); // displays the shortcut
+
+		JMenu toolsMenu = new JMenu("Tools");
 		JMenuItem uploadItem = new JMenuItem("Upload");
 		toolsMenu.add(uploadItem);
-		
-        menuBar.add(fileMenu);
-        menuBar.add(toolsMenu);
-		
 
-        loadFile.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                    System.out.println(fileChooser.getSelectedFile());
-                    loadFile();
-                }
-            }
-        });
+		toolsMenu.setMnemonic(KeyEvent.VK_T);
+		uploadItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U,
+				ActionEvent.ALT_MASK));
 
-        createFile.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
-                    System.out.println(fileChooser.getSelectedFile());
-                    createFile();
-                }
-            }
-        });
+		menuBar.add(fileMenu);
+		menuBar.add(toolsMenu);
 
-        exitItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
+		loadFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+					System.out.println(fileChooser.getSelectedFile());
+					loadFile();
+				}
+			}
+		});
 
-                int action = JOptionPane.showConfirmDialog(null,
-                        "Do you really want to exit the application?",
-                        "Confirm Exit", JOptionPane.OK_CANCEL_OPTION);
+		createFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+					System.out.println(fileChooser.getSelectedFile());
+					createFile();
+				}
+			}
+		});
 
-                if (action == JOptionPane.OK_OPTION) {
-                    System.exit(0);
-                }
-            }
-        });
-        
-        uploadItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-//                System.out.println("testing");
-                Upload upload = new Upload();
-               
-            }
-        });
+		exitItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 
-        return menuBar;
+				int action = JOptionPane.showConfirmDialog(null,
+						"Do you really want to exit the application?",
+						"Confirm Exit", JOptionPane.OK_CANCEL_OPTION);
 
-    }
+				if (action == JOptionPane.OK_OPTION) {
+					System.exit(0);
+				}
+			}
+		});
 
-    // creates a file in a chosen format and saves it to the selected folder
-    public void createFile() {
-        File selectedFolder = fileChooser.getSelectedFile(); // this is the
-                                                                // selected
-                                                                // destination
-        try {
-            FileWriter writer = new FileWriter(selectedFolder); // saves the
-                                                                // file to the
-                                                                // selected
-                                                                // folder
-            writer.close(); // the writer has to be closed
-        } catch (IOException e1) {
-            System.out.println(e1.getMessage());
-            // displays a pop up with an error message if the file can not be
-            // saved
-            JOptionPane
-                    .showMessageDialog(fileChooser,
-                            "Unable to save file " + e1.getMessage(),
-                            "Save Dialog", JOptionPane.ERROR_MESSAGE);
-        }
-    }
+		uploadItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// System.out.println("testing");
+				Upload upload = new Upload();
 
-    public void loadFile() {
+			}
+		});
 
-        File chosenFile = fileChooser.getSelectedFile();
+		return menuBar;
 
-        try {
-            Reader reader = new FileReader(chosenFile);
-            reader.close(); // closing the reader
-            String[] cmd = new String[5];
-            cmd[0] = "cmd.exe";
-            cmd[1] = "/C";
-            cmd[2] = "rundll32";
-            cmd[3] = "url.dll,FileProtocolHandler";
-            cmd[4] = chosenFile.toString();
+	}
 
-            Runtime.getRuntime().exec(cmd); // opens the file in the application
-                                            // the file is to be open from, e.g.
-                                            // xls-excel
+	// creates a file in a chosen format and saves it to the selected folder
+	public void createFile() {
+		File selectedFolder = fileChooser.getSelectedFile(); // this is the
+																// selected
+																// destination
+		try {
+			FileWriter writer = new FileWriter(selectedFolder); // saves the
+																// file to the
+																// selected
+																// folder
+			writer.close(); // the writer has to be closed
+		} catch (IOException e1) {
+			System.out.println(e1.getMessage());
+			// displays a pop up with an error message if the file can not be
+			// saved
+			JOptionPane
+					.showMessageDialog(fileChooser,
+							"Unable to save file " + e1.getMessage(),
+							"Save Dialog", JOptionPane.ERROR_MESSAGE);
+		}
+	}
 
-        } catch (FileNotFoundException e) {
-            System.out.println("The file is not found " + e.getMessage());
-        } catch (IOException eio) {
-            System.out.println("An error has occured: " + eio.getMessage());
-        }
-    }
+	public void loadFile() {
+
+		File chosenFile = fileChooser.getSelectedFile();
+
+		try {
+			Reader reader = new FileReader(chosenFile);
+			reader.close(); // closing the reader
+			String[] cmd = new String[5];
+			cmd[0] = "cmd.exe";
+			cmd[1] = "/C";
+			cmd[2] = "rundll32";
+			cmd[3] = "url.dll,FileProtocolHandler";
+			cmd[4] = chosenFile.toString();
+
+			Runtime.getRuntime().exec(cmd); // opens the file in the application
+											// the file is to be open from, e.g.
+											// xls-excel
+
+		} catch (FileNotFoundException e) {
+			System.out.println("The file is not found " + e.getMessage());
+		} catch (IOException eio) {
+			System.out.println("An error has occured: " + eio.getMessage());
+		}
+	}
 }
-
-
--- FinnishGame/src/Upload.java
+++ FinnishGame/src/Upload.java
@@ -1,11 +1,17 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 
 public class Upload {
 	JDialog dialog = new JDialog();
@@ -16,6 +22,7 @@ public class Upload {
 			{ "2r - 1c", "2r - 2c" },
 			{ "3r - 1c", "3r - 2c" } };
 	JTable table = new JTable(data, columnNames);
+	JButton save = new JButton("Save");
 
 	public Upload() {
 
@@ -26,13 +33,10 @@ public class Upload {
 
 		table.setRowHeight(40);
 		table.setCellSelectionEnabled(true);
-		// table.getColumnModel().getColumn(1).setCellRenderer(new
-		// CustomCellRenderer());
-		// table.getColumnModel().getColumn(1).setCellEditor(new
-		// CustomEditor());
-
 		u_panel.add(table);
 		u_panel.add(u_upload);
+		u_panel.add(save);
+		saveFile();
 		
 		u_upload.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -44,6 +48,33 @@ public class Upload {
 	public boolean isCellEditable(int rowIndex, int columnIndex)
 	{
 	    return true;
+	}
+	public void saveFile(){
+		save.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setDialogTitle("Specify a file to save");
+
+				int userSelection = fileChooser.showSaveDialog(null);
+
+				if (userSelection == JFileChooser.APPROVE_OPTION) {
+					File fileToSave = fileChooser.getSelectedFile();
+					System.out.println("Save as file: "
+							+ fileToSave.getAbsolutePath());
+					try (FileWriter fw = new FileWriter(fileChooser
+							.getSelectedFile())) {
+						for (Object x : data) {
+							//fw.write(x.toString());
+							System.out.println(x);
+
+						}
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
 	}
 	public Object getValueAt(int row, int col) { 
 		return data[row][col];
