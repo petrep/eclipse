47ef7b8714b11d82aad36dcb2a20f7a71b229c37
Peter Peter <petrep@freemail.hu>
2015-10-11 20:27:44 +0200

    enter method is working columns get created by clicking the enter key

--- FinnishGame/src/Upload.java
+++ FinnishGame/src/Upload.java
@@ -1,5 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -12,6 +14,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 public class Upload {
 	JDialog dialog = new JDialog();
@@ -20,7 +24,7 @@ public class Upload {
 	String[] columnNames = { "English", "Finnish" };
 	String[][] data = { { "1r - 1c", "1r - 2c" }, { "2r - 1c", "2r - 2c" },
 			{ "3r - 1c", "3r - 2c" } };
-	JTable table = new JTable(data, columnNames);
+	JTable table = new JTable(new DefaultTableModel(data, columnNames));
 	JButton save = new JButton("Save");
 
 	public Upload() {
@@ -29,13 +33,15 @@ public class Upload {
 		dialog.setVisible(true);
 		dialog.setTitle("Upload new words into the database");
 		dialog.add(u_panel);
-
+		
 		table.setRowHeight(40);
 		table.setCellSelectionEnabled(true);
 		u_panel.add(u_upload);
 		u_panel.add(save);
-		u_panel.add(new JScrollPane(table)); // displays the headers of the table
+		u_panel.add(new JScrollPane(table)); // displays the headers of the
+												// table
 		saveFile();
+		enter();
 
 		u_upload.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -66,7 +72,7 @@ public class Upload {
 							+ fileChooser.getSelectedFile());
 					savingPath = fileChooser.getSelectedFile();
 				}
-				
+
 				PrintWriter writer = null;
 				// Saving the English words
 				try {
@@ -78,28 +84,29 @@ public class Upload {
 					JOptionPane.showMessageDialog(fileChooser,
 							"Unable to save file " + e1.getMessage(),
 							"Save Dialog", JOptionPane.ERROR_MESSAGE);
-					
+
 				} catch (UnsupportedEncodingException e1) {
 					JOptionPane.showMessageDialog(fileChooser,
-							"Unable to save file because the text contains unsupported characters " + e1.getMessage(),
-							"Save Dialog", JOptionPane.ERROR_MESSAGE);
-					
+							"Unable to save file because the text contains unsupported characters "
+									+ e1.getMessage(), "Save Dialog",
+							JOptionPane.ERROR_MESSAGE);
+
 				}
 				if (writer != null) { // checks if the file has been created
 					writer.println("The first line");
 					writer.println("The second line");
-					//for (Object x : data) {
+					// for (Object x : data) {
 					// fw.write(x.toString());
-					for (int englishRowCount = 0; englishRowCount <= table.getRowCount()-1; englishRowCount++){
+					for (int englishRowCount = 0; englishRowCount <= table
+							.getRowCount() - 1; englishRowCount++) {
 						System.out.println(getValueAt(englishRowCount, 0));
 						writer.println(getValueAt(englishRowCount, 0));
 					}
-					
 
-				//}
+					// }
 					writer.close();
 				}
-				
+
 				// Saving the Finnish words
 				try {
 					writer = new PrintWriter(savingPath + File.separator
@@ -110,24 +117,24 @@ public class Upload {
 					JOptionPane.showMessageDialog(fileChooser,
 							"Unable to save file " + e1.getMessage(),
 							"Save Dialog", JOptionPane.ERROR_MESSAGE);
-					
+
 				} catch (UnsupportedEncodingException e1) {
 					JOptionPane.showMessageDialog(fileChooser,
-							"Unable to save file because the text contains unsupported characters " + e1.getMessage(),
-							"Save Dialog", JOptionPane.ERROR_MESSAGE);
-					
+							"Unable to save file because the text contains unsupported characters "
+									+ e1.getMessage(), "Save Dialog",
+							JOptionPane.ERROR_MESSAGE);
+
 				}
 				if (writer != null) { // checks if the file has been created
 					writer.println("The first line");
 					writer.println("The second line");
-					for (int finnishRowCount = 0; finnishRowCount <= table.getRowCount()-1; finnishRowCount++){
+					for (int finnishRowCount = 0; finnishRowCount <= table
+							.getRowCount() - 1; finnishRowCount++) {
 						System.out.println(getValueAt(finnishRowCount, 1));
 						writer.println(getValueAt(finnishRowCount, 1));
 					}
 					writer.close();
 				}
-				
-				
 
 			}
 
@@ -137,10 +144,18 @@ public class Upload {
 	public Object getValueAt(int row, int col) {
 		return data[row][col];
 	}
-public void enter(){
-	
-	
-}
+
+	public void enter() {
+		table.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					DefaultTableModel model = (DefaultTableModel) table.getModel();
+					model.addRow(new Object[]{"Column 1", "Column 2"});
+					System.out.println("test");
+				}
+			}
+		});
+	}
 	// URL url = new URL("ftp://user:pass@myftp.abc.com/myFile.txt;type=i");
 	// URLConnection urlc = url.openConnection();
 	// OutputStream os = urlc.getOutputStream(); // To upload
